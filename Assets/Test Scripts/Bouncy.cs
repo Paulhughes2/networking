b46e08f9c34ec63a18105e7d1c13360618367745
Paulhughes2 <plhgh118@hotmail.com>
2018-06-17 21:25:32 +0100

    testgrounds

---
+++ Assets/Test Scripts/Bouncy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour {
+
+	public float MinDistance = 2.0f;
+	public float MaxDistance = 5.0f;
+	public float MaxForce = 15.0f;
+
+
+	float RaycastDownwardsFromMe()
+	{
+		RaycastHit rch;
+		if (Physics.Raycast ( transform.position, Vector3.down, out rch, MaxDistance))
+		{
+			Debug.DrawRay (transform.position, Vector3.down * rch.distance, Color.green);
+			return rch.distance;
+		}
+		return 100;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+		float distance = RaycastDownwardsFromMe();
+		float fractionalPosition = (MaxDistance - distance) / (MaxDistance - MinDistance);
+		if (fractionalPosition < 0) fractionalPosition = 0;
+		if (fractionalPosition > 1) fractionalPosition = 1;
+		float force = fractionalPosition * MaxForce;
+		gameObject.GetComponent<Rigidbody>().AddForce( Vector3.up * force);
+
+	}
+}
---
+++ Assets/Test Scripts/Floaty.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floaty : MonoBehaviour {
+
+	public float maxDistance = 0.5f;
+	RaycastHit toFloor;
+
+		// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		
+		if (Physics.Raycast ( transform.position, Vector3.down, out toFloor, 30f))
+		{
+			Debug.DrawRay (transform.position, Vector3.down * toFloor.distance, Color.green);
+			
+		}
+		
+		if(toFloor.distance > maxDistance){
+			transform.localPosition = new Vector3(transform.localPosition.x, (toFloor.point.y + maxDistance),transform.localPosition.z);
+		}
+	}
+}
